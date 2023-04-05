352a34001a022b3774d063d937ccafc0c76478dd
electrolyteJ <hawks.jamesf@gmail.com>
2023-04-05 08:08:43 +0800

    u

--- instance/prod_config.py
+++ instance/prod_config.py
@@ -2,3 +2,4 @@ ENV = 'prod'
 DEBUG = False
 DB_SERVER = 'localhost'
 SECRET_KEY = b'\xfd\xdec\xf4+\r\xef\x81#Qe\xc2\xad5\x89\xa2'
+MONGO_URI = "mongodb://mongo:p5b67avO9383spNYAXYt@containers-us-west-32.railway.app:5746"
